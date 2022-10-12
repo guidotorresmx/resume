073a71081d0a056a75db9bc3fbbefc1017197a85
Marco Guido <guido-torres@hotmail.com>
2022-10-12 15:12:39 -0400

    added initial scale change

---
+++ app/js/animate-resume.js
@@ -0,0 +1,6 @@
+function animateResume() {
+  let resume = document.querySelector(".resume");
+  resume.style.transform = "scale(1,1)"
+
+}
+export default animateResume;
---
+++ docs/js/animate-resume.js
@@ -0,0 +1,7 @@
+function animateResume() {
+  let resume = document.querySelector(".resume");
+  resume.style.transform = "scale(1,1)"
+
+}
+export default animateResume;
+//# sourceMappingURL=animate-resume.js.map
