09f962e70451ebfd2961f3f6220f9a38c39e9fb7
Marco Guido <guido-torres@hotmail.com>
2022-10-13 01:30:07 -0400

    added article animation

--- app/js/animate-resume.js
+++ app/js/animate-resume.js
@@ -1,6 +1,8 @@
 function animateResume() {
   let resume = document.querySelector(".resume");
   resume.style.transform = "scale(1,1)"
+  resume.style.opacity = "1"
+
 
 }
 export default animateResume;
